b542a43c3831d64da6a8b42c50edd99a93748c75
mihirsatra44 <56023920+mihirsatra44@users.noreply.github.com>
2020-07-08 18:19:36 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const city = req.body.cityName;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=85bba9630f3d59b8c5c17d8b0bbae905&units=metric";
+  https.get(url, function(response) {
+
+    response.on("data", function(data) {
+      const weatherdata = JSON.parse(data);
+      const temp = weatherdata.main.temp;
+      const weatherdes = weatherdata.weather[0].description;
+      const icon = weatherdata.weather[0].icon;
+      const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<h1>The weather in " + city + " is " + temp + " degrees.");
+      res.write("<h3>Weather Description:  " + weatherdes + "</h3>");
+      res.write("<img src=" + imageUrl + ">");
+      res.send();
+    });
+  });
+});
+
+
+
+app.listen(3000, function(req, res) {
+  console.log("Server started.");
+});
